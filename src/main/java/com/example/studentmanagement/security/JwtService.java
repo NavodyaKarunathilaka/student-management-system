7dea4963184eac8455257b41d9818529052b4bb9
Navodya Karunathilaka <navodyakavindi1013@gmail.com>
2026-08-05 01:56:51 +0530

    feat(auth): implement basic authentication module

---
+++ src/main/java/com/example/studentmanagement/security/JwtService.java
@@ -0,0 +1,29 @@
+package com.example.studentmanagement.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    // Secret key (temporary)
+    private static final String SECRET =
+            "studentmanagementsystemsecretkey12345678901234567890";
+
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
